abc306883b9f19f40563a88c867cefebcbc1e360
mesahilansari <ahilansari842@gmail.com>
2025-08-14 15:31:48 +0530

    version 1

--- insurance.py
+++ insurance.py
@@ -29,6 +29,7 @@ pipeline = Pipeline([
     ("model", LinearRegression())
 ])
 
+
 # Step 5: Split and train
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 pipeline.fit(X_train, y_train)
